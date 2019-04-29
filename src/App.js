a4a4fea27ce38d27a198ab4c0b49e66194325f51
Kethy Wang <kethyxwang@gmail.com>
2019-04-28 18:48:10 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './css/main.css';
+
+function App() {
+  return (
+
+
+    <div className="App">
+      <header className="App-header">
+
+        <a className="grow w-70"> <img src="ardent.png" alt="ardent" /></a>
+
+        <p className="f4-l f5 tracked">
+          print issue one...work in progress
+        </p>
+        <a className="ardent-color tracked no-underline dim f5" rel="noopener noreferrer" href="https://readymag.com/u78931358/online-issue-one/" target="_blank">view online issue one</a>
+        <br />
+        <a className="f6 white-50 dim no-underline" href="mailto:ardentmag@protonmail.com">ardentmag@protonomail</a>
+
+
+        <a className="f6 no-underline dim white-50" href='https://www.instagram.com/ardentmag'> instagram</a>
+
+
+
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
